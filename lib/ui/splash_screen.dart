504df80e5dfdfb2c0427c311e94662fde7e0a8b3
syymm <shiyuming233@gmail.com>
2025-03-03 23:05:57 +0900

    Initial commit: Add splash screen

---
+++ lib/ui/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // 延迟3秒后导航到主页面
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.of(context).pushReplacementNamed('/home');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(24), // 设置圆角大小
+          child: SvgPicture.asset(
+            'assets/images/app_logo.svg',
+            width: 120,
+            height: 120,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
